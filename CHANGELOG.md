33bff77798616c72afd5273d170a92d8d033dabc
Kevin Sylvestre <kevin@ksylvest.com>
2018-02-15 15:33:38 -0800

    use a ‘default_changer_proc’ via config

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+# Changed
+
+All notable changes to this project will be documented in this file.
+
+## [v1.0.0] - Thursday, February 15, 2018
+
+ - Enabling changed configuration for the `Changed.config.default_changer_proc`.
+ - Restructuring `Changed.config` to `Changed.perform`
+
+## [v0.1.0] - Thursday, February 15, 2018
+
+ - Setup `Changed::Audit`, `Changed::Association` for all things changed.
+ - Migrated over default set of specifications and libraries into engine.
+ - Configuration Circle CI, RSpec, SimpleCov, etc.
--- lib/changed.rb
+++ lib/changed.rb
@@ -2,29 +2,72 @@ require 'request_store'
 
 require 'changed/auditable'
 require 'changed/builder'
+require 'changed/config'
 require 'changed/engine'
 
 module Changed
   Field = Struct.new(:was, :now, :name)
   Relationship = Struct.new(:associations, :name)
 
+  # Access the library configuration.
+  #
+  # ==== Examples
+  #
+  #    Changed.config.default_changer_proc = ->{ User.system }
+  def self.config
+    @config ||= Config.new
+  end
+
+  # Access the timestamp (this value is set as the timestamp within an audit and defaults to now).
   def self.timestamp
     options[:timestamp] || Time.now
   end
 
+  # Customize the timestamp (uses a request store to only change lifeycle event).
+  #
+  # ==== Attributes
+  #
+  # * +timestamp+ - A timestamp to use.
+  #
+  # ==== Examples
+  #
+  #    Changed.timestamp = 2.hours.ago
   def self.timestamp=(timestamp)
     options[:timestamp] = timestamp
   end
 
+  # Access the changer (this value is set as the changer within an audit and defaults to config).
   def self.changer
-    options[:changer]
+    options[:changer] || config.default_changer_proc&.call
   end
 
+  # Customize the changer (uses a request store to only change lifeycle event).
+  #
+  # ==== Attributes
+  #
+  # * +changer+ - A changer to use.
+  #
+  # ==== Examples
+  #
+  #    Changed.changer = User.current
   def self.changer=(changer)
     options[:changer] = changer
   end
 
-  def self.config(options = {}, &block)
+  # Perform a block with custom override options.
+  #
+  # ==== Attributes
+  #
+  # * +options+ - Values for the changer and / or timestamp.
+  # * +block+ - Some code to run with the new options.
+  #
+  # ==== Examples
+  #
+  #    Changed.perform(changer: User.system, timestamp: 2.hours.ago) do
+  #      widget.name = "Sprocket"
+  #      widget.save!
+  #    end
+  def self.perform(options = {}, &block)
     backup = self.options
     self.options = options
     block.call
