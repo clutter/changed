ae11b50505f2f83595b04333a85c4096daffaff4
Kevin Sylvestre <kevin@ksylvest.com>
2018-03-05 10:51:59 -0800

    default to factory bot for fixture replacement

--- lib/changed/engine.rb
+++ lib/changed/engine.rb
@@ -4,6 +4,7 @@ module Changed
 
     config.generators do |generator|
       generator.test_framework :rspec
+      generator.fixture_replacement :factory_bot, dir: 'spec/factories'
     end
   end
 end
