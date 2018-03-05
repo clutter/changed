f9593c3d5b0987e46c7a6961d8ac7a26f31b49a5
Kevin Sylvestre <kevin@ksylvest.com>
2018-03-05 10:52:26 -0800

    standardized spec formatting

--- spec/models/changed/association_spec.rb
+++ spec/models/changed/association_spec.rb
@@ -1,11 +1,9 @@
 require 'spec_helper'
 
-module Changed
-  RSpec.describe Association, type: :model do
-    it do
-      should belong_to :audit
-      should belong_to :associated
-      should validate_presence_of :name
-    end
+RSpec.describe Changed::Association, type: :model do
+  it do
+    should belong_to :audit
+    should belong_to :associated
+    should validate_presence_of :name
   end
 end
